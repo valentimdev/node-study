7e9dcd2c078dd8ecf511666096354931cac9fd52
ALUNO C24 <alunoc24@acad.unifor.br>
2024-09-18 20:09:38 -0300

    first commit

---
+++ teste.js
@@ -0,0 +1,35 @@
+const express = require("express"); // importa o modulo epress neste arqivo
+const app = express();//iniciando o express
+
+app.get("/",function(req,res){
+    res.send("Bem vindo ao meu site!");
+})
+app.get("/produtos",function(req,res){//rota de cadastro dos produtos
+    res.send("<h1>Lista de produtos!</h1>");
+})
+
+app.listen(4000,function(erro){//cria aplicacao na porta 4000
+    if(erro){
+        console.log("Erro ao iniciar.")
+    }else{
+        console.log("Servidor Iniciado.")
+    }
+
+})
+//rota com parametro
+app.get("/consulta/:parametro",function(req,res){
+    //req->dados enviados pelo cliente
+    //req->resposta enviada pelo servidor  de volta ao cliente
+    res.send("retorno consulta:"+req.params.parametro)
+})
+//rota com parametro adicional
+app.get("/cadastro/:nome?",function(req,res){
+    //req->dados enviados pelo cliente
+    var nome=req.params.nome;
+    if(nome){
+        res.send("<h1>produto "+nome+" criado</h1>");
+    }else{
+        res.send("produto criado!")
+    }
+})
+
